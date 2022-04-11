ad38a0fadda7adb8ab3c4ea164f86e06dd8d44ce
Jett Thompson <jett.thompson.59@gmail.com>
2022-04-11 10:15:46 +1000

    made a way of generating a random neural network

--- src/neurons.cpp
+++ src/neurons.cpp
@@ -1,17 +1,20 @@
 #include "neurons.h"
+#include "random.h"
 #include <cstddef>
 #include <utility>
 #include <vector>
 
+using random::Random;
 using std::move;
 using std::vector;
 
 namespace neurons {
 vector<double> Layer::apply(const vector<double> &inputs,
                             ActivationFunction &activationFunction,
-                            const Layer &nextLayer) const {
-  vector<double> result(nextLayer.neuronCount());
-  for (size_t i = 0; i < nextLayer.neuronCount(); i++) {
+                            size_t nextLayerSize) const {
+  vector<double> result;
+  result.reserve(nextLayerSize);
+  for (size_t i = 0; i < nextLayerSize; i++) {
     double value = 0;
     for (size_t j = 0; j < neuronCount(); i++) {
       const Neuron &neuron = neurons[j];
@@ -25,8 +28,41 @@ vector<double> Network::apply(const vector<double> &inputs,
                               ActivationFunction &activationFunction) const {
   vector<double> temp = inputs;
   for (size_t i = 0; i < layerCount() - 1; i++) {
-    temp = layers[i].apply(temp, activationFunction, layers[i + 1]);
+    temp =
+        layers[i].apply(temp, activationFunction, description.layerSize(i + 1));
   }
   return temp;
+}
+
+Neuron NetworkDescription::generateNeuron(Random &rand,
+                                          size_t weightCount) const {
+  vector<double> weights;
+  weights.reserve(weightCount);
+  vector<double> biases;
+  biases.reserve(weightCount);
+  for (size_t i = 0; i < weightCount; i++) {
+    weights.push_back(rand.getDouble());
+    biases.push_back(rand.getDouble());
+  }
+  return Neuron(weights, biases);
+}
+Layer NetworkDescription::generateLayer(Random &rand, size_t layerSize,
+                                        size_t nextLayerSize) const {
+  vector<Neuron> neurons;
+  neurons.reserve(layerSize);
+  for (size_t i = 0; i < layerSize; i++) {
+    neurons.push_back(generateNeuron(rand, nextLayerSize));
+  }
+  return Layer(neurons);
+}
+Network NetworkDescription::generateNetwork(Random &rand) const {
+  size_t layerCount = layerDescriptions.size();
+  vector<Layer> layers;
+  layers.reserve(layerCount - 1);
+  for (size_t i = 0; i < layerCount - 1; i++) {
+    layers.push_back(
+        generateLayer(rand, layerDescriptions[i], layerDescriptions[i + 1]));
+  }
+  return Network(*this, layers);
 }
 } // namespace neurons
--- src/neurons.h
+++ src/neurons.h
@@ -1,6 +1,7 @@
 #ifndef _NEURONS_H
 #define _NEURONS_H
 
+#include "random.h"
 #include <cstddef>
 #include <functional>
 #include <stdexcept>
@@ -9,6 +10,27 @@
 
 namespace neurons {
 using ActivationFunction = const std::function<double(double)>;
+
+class Neuron;
+class Layer;
+class Network;
+
+class NetworkDescription {
+private:
+  std::vector<size_t> layerDescriptions;
+
+  Neuron generateNeuron(random::Random &rand, size_t weightCount) const;
+  Layer generateLayer(random::Random &rand, size_t layerSize,
+                      size_t nextLayerSize) const;
+
+public:
+  NetworkDescription(std::vector<size_t> layers)
+      : layerDescriptions(std::move(layers)) {}
+
+  size_t layerSize(size_t index) const { return layerDescriptions[index]; }
+
+  Network generateNetwork(random::Random &rand) const;
+};
 
 class Neuron {
 private:
@@ -45,17 +67,21 @@ public:
   // Returns the values for the next layer
   std::vector<double> apply(const std::vector<double> &input,
                             ActivationFunction &activationFunction,
-                            const Layer &nextLayer) const;
+                            size_t nextLayerSize) const;
 };
 class Network {
 private:
+  const NetworkDescription description;
   const std::vector<Layer> layers;
 
 public:
-  Network(const std::vector<Layer> layers) : layers(std::move(layers)) {}
+  Network(const NetworkDescription description, const std::vector<Layer> layers)
+      : description(std::move(description)), layers(std::move(layers)) {}
 
   const Layer &getLayer(size_t index) const { return layers[index]; }
   size_t layerCount() const { return layers.size(); }
+
+  size_t layerSize(size_t index) const { return description.layerSize(index); }
 
   // Returns the result of applying the inputs
   std::vector<double> apply(const std::vector<double> &inputs,
